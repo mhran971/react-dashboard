e49224d46a363bebff539c0527f759e5c5c57508
Mahran_AD <mhranabwdqt971@gmail.com>
2025-10-19 03:38:56 +0300

    `Added new route for users under Dashboard`

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Dashboard" element={<Dashboard />}>
           <Route  path="users" element={<Users />} />
+          
         </Route>
       </Routes>
     </div>
